911fa9c78d7a37aca1fcb4e507515be088339eeb
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-10 15:09:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeDetails) {
+    return {
+        firstName: employeeDetails[0],
+        familyName: employeeDetails[1],
+        title: employeeDetails[2],
+        payPerHour: employeeDetails[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(employeeRecords) {
+    return employeeRecords.map(record => createEmployeeRecord(record))
+}
+
+function createTimeInEvent(dateStamp) {
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+    return this
+}
+
+function createTimeOutEvent( dateStamp) {
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    let dates = this.timeInEvents.map( e => e.date)
+    let index = dates.indexOf(dateStamp)
+    return ((this.timeOutEvents[index].hour - this.timeInEvents[index].hour) / 100)
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    return hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour
+}
+
+function calculatePayroll(formattedEmployeeRecords) {
+    let totalPay = 0
+    formattedEmployeeRecords.forEach(employeeRecord => {
+        totalPay += allWagesFor.call(employeeRecord)
+    })
+    return totalPay
+}
+
+function findEmployeeByFirstName(employeeRecords, firstNameString) {
+    let matches = employeeRecords.filter( e => e.firstName === firstNameString)
+    return (matches.length > 0) ? matches[0] : undefined
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
